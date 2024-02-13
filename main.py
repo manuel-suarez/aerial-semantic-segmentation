08323ed94bb8b34f6cbb2b52391d0c2383dc066c
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-13 12:55:16 -0600

    Import definition

--- main.py
+++ main.py
@@ -27,3 +27,16 @@ Total 1305 patches of size 256x256
 
 """
 
+import os
+import cv2
+import numpy as np
+
+from matplotlib import pyplot as plt
+from patchify import patchify
+from PIL import Image
+import segmentation_models as sm
+from tensorflow.keras.metrics import MeanIoU
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+scaler = MinMaxScaler()
+
