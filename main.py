1c1106c3604dee61b1b2ed15518349662283771c
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-13 13:10:00 -0600

    Load dataset and preview figure

--- main.py
+++ main.py
@@ -42,3 +42,80 @@ scaler = MinMaxScaler()
 
 root_directory = 'Semantic segmentation dataset/'
 patch_size = 256
+
+# Read images from respective 'images' subdirectory
+# As all images are of different size we have 2 options, either resize or crop
+# However, some images are too large and some small so, resizing will change the size of real objects
+# Therefore, we will crop them to a nearest size divisible by 256 and then divide all images into patches
+image_dataset = []
+for path, subdirs, files in os.walk(root_directory):
+    dirname = path.split(os.path.sep)[-1]
+    if dirname == 'images':
+        images = os.listdir(path)
+        for i, image_name in enumerate(images):
+            if image_name.endswith(".jpg"):
+                image = cv2.imread(os.path.join(path, image_name), 1) # Read as BGR
+                SIZE_X = (image.shape[1]//patch_size)*patch_size
+                SIZE_Y = (image.shape[0]//patch_size)*patch_size
+                image = Image.fromarray(image)
+                image = image.crop((0, 0, SIZE_X, SIZE_Y))
+                image = np.array(image)
+                # Extract patches from each image
+                print("Now patchifying image:", path+"/"+image_name)
+                patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)
+                for i in range(patches_img.shape[0]):
+                    for j in range(patches_img.shape[1]):
+                        single_patch_img = patches_img[i,j,:,:]
+                        # Use minmaxscaler instead of just dividing by 255.
+                        single_patch_img = scaler.fit_transform(
+                            single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
+                        single_patch_img = single_patch_img[0]
+                        image_dataset.append(single_patch_img)
+
+# Now do the same as above for masks
+# For this specific dataset we could have added masks to the above code as masks have extension png
+mask_dataset = []
+for path, subdirs, files in os.walk(root_directory):
+    # print(path)
+    dirname = path.split(os.path.sep)[-1]
+    if dirname == 'masks':  # Find all 'images' directories
+        masks = os.listdir(path)  # List of all image names in this subdirectory
+        for i, mask_name in enumerate(masks):
+            if mask_name.endswith(".png"):  # Only read png images... (masks in this dataset)
+
+                mask = cv2.imread(path + "/" + mask_name,
+                                  1)  # Read each image as Grey (or color but remember to map each color to an integer)
+                mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+                SIZE_X = (mask.shape[1] // patch_size) * patch_size  # Nearest size divisible by our patch size
+                SIZE_Y = (mask.shape[0] // patch_size) * patch_size  # Nearest size divisible by our patch size
+                mask = Image.fromarray(mask)
+                mask = mask.crop((0, 0, SIZE_X, SIZE_Y))  # Crop from top left corner
+                # mask = mask.resize((SIZE_X, SIZE_Y))  #Try not to resize for semantic segmentation
+                mask = np.array(mask)
+
+                # Extract patches from each image
+                print("Now patchifying mask:", path + "/" + mask_name)
+                patches_mask = patchify(mask, (patch_size, patch_size, 3),
+                                        step=patch_size)  # Step=256 for 256 patches means no overlap
+
+                for i in range(patches_mask.shape[0]):
+                    for j in range(patches_mask.shape[1]):
+                        single_patch_mask = patches_mask[i, j, :, :]
+                        # single_patch_img = (single_patch_img.astype('float32')) / 255. #No need to scale masks, but you can do it if you want
+                        single_patch_mask = single_patch_mask[
+                            0]  # Drop the extra unecessary dimension that patchify adds.
+                        mask_dataset.append(single_patch_mask)
+
+image_dataset = np.array(image_dataset)
+mask_dataset = np.array(mask_dataset)
+
+# Sanity check
+import random
+import numpy as np
+image_number = random.randint(0, len(image_dataset))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(np.reshape(image_dataset[image_number], (patch_size, patch_size, 3)))
+plt.subplot(122)
+plt.imshow(np.reshape(mask_dataset[image_number], (patch_size, patch_size, 3)))
+plt.show()
